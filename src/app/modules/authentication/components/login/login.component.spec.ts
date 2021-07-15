723cabb99a5006446a3c83679b481fff9cba7c9d
Chandan Mishra <chandanmishra3706@gmail.com>
2021-07-16 00:55:13 +0530

    Form validation test cases added for LoginComponent

--- src/app/modules/authentication/components/login/login.component.spec.ts
+++ src/app/modules/authentication/components/login/login.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../../../shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '../../../material/material.module';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -9,6 +15,15 @@ describe('LoginComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        ReactiveFormsModule,
+        FormsModule,
+        SharedModule,
+        BrowserAnimationsModule,
+        MaterialModule,
+      ],
     }).compileComponents();
   });
 
@@ -18,7 +33,39 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('LoginComponent should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('[Email Valid] - Email should be valid',  () => {
+    let emailControl = component.loginForm.controls.email;
+    emailControl.setValue('abc@abc.com');
+    expect(emailControl.value).toBe('abc@abc.com');
+    expect(emailControl.valid).toBeTrue();
+    expect(emailControl.errors).toBeNull();
+    expect(emailControl.hasError('email')).toBeFalse();
+  });
+
+  it('[Email Invalid] - Email should be Invalid',  () => {
+    let emailControl = component.loginForm.controls.email;
+    emailControl.setValue('abc');
+    expect(emailControl.value).toBe('abc');
+    expect(emailControl.valid).toBeFalse();
+    expect(emailControl.hasError('email')).toBeTrue();
+  });
+
+  it('[Email Required] - Email should be required should pass',  () => {
+    let emailControl = component.loginForm.controls.email;
+    emailControl.setValue('abc@abc.com');
+    expect(emailControl.value).toBe('abc@abc.com');
+    expect(emailControl.valid).toBeTrue();
+    expect(emailControl.hasError('required')).toBeFalse();
+  });
+
+  it('[Email Required Fail] - Email should be required should fail',  () => {
+    let emailControl = component.loginForm.controls.email;
+    emailControl.setValue('');
+    expect(emailControl.invalid).toBeTrue();
+    expect(emailControl.hasError('required')).toBeTrue();
   });
 });
--- src/app/modules/authentication/components/login/login.component.ts
+++ src/app/modules/authentication/components/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {
-  FormBuilder,
   FormControl,
   FormGroup,
   Validators,
